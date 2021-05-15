88811963dee1d277ea73621472d1fd5c5b3d6e0b
marik <marikandris.97gmail.com>
2021-05-15 14:37:53 +0200

    market_checkcer.py finalized

--- App/market_checker.py
+++ App/market_checker.py
@@ -10,7 +10,7 @@ def market_check():
     user = request_try.try_request_get(vars.users_URL, {'id': vars.user_id})
 
     current_history = user[0]['history']
-    if players_on_market or current_history:
+    if current_history:
         players_expired = []
         for item in players_on_market:
             expire_date = datetime.strptime(item['expire'], '%d/%m/%Y %H:%M:%S')
@@ -21,7 +21,7 @@ def market_check():
             players_by_market_id = request_try.try_request_get(vars.market_URL, {'id': current_history})
             display.show_history(players_by_market_id)
             if players_expired:
-                current_history = reserve_team_update(players_expired, current_history, user[0]['owned_players'])
+                current_history = reserve_team_update(players_expired, current_history, user[0]['owned_players'], user[0]['starting_11'])
             history_patched = request_try.try_request_patch(vars.users_id_url, {'history': current_history})
             return True
     else:
@@ -29,16 +29,30 @@ def market_check():
         return False
 
 
-def reserve_team_update(expired_players, history, user_owned_players_id):
+def reserve_team_update(expired_players, history, user_owned_players_id, user_starting_11):
+    user_owned_players = request_try.try_request_get(vars.players_URL, {'futbin_id': user_owned_players_id})
+    user_starting_11_id = list(user_starting_11.values())
+    user_starting_11 = request_try.try_request_get(vars.players_URL, {'futbin_id': user_starting_11_id})
+
     for player in expired_players:
-        if int(player['futbin_id']) in user_owned_players_id:
-            relisted = sell.relist(player)
-            if relisted:
-                display.print_info_green("Player relisted on the market successfully!")
-        else:
+        is_duplicate = False
+        for owned_player in user_owned_players:
+            if player['player_extended_name'] == owned_player['player_extended_name']:
+                is_duplicate = True
+                relisted = sell.relist(player)
+                if relisted:
+                    display.print_info_green("Player relisted on the market successfully!")
+        for starting_player in user_starting_11:
+            if player['player_extended_name'] == starting_player['player_extended_name']:
+                is_duplicate = True
+                relisted = sell.relist(player)
+                if relisted:
+                    display.print_info_green("Player relisted on the market successfully!")
+        if not is_duplicate:
             user_owned_players_id.append(int(player['futbin_id']))
             if player['id'] in history:
                 history.remove(player['id'])
+
     owned = user_owned_players_id
     request_try.try_request_patch(vars.users_id_url, {'owned_players': owned})
     return history
--- App/vars.py
+++ App/vars.py
@@ -32,8 +32,3 @@ def set_user_vars(url, id):
     global users_id_url
     users_id_url = url
     user_id = id
-
-
-"""
-4418,      23859,       12676,
-"""
